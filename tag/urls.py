b2302e088243201b22b557bcf46a0ce34575478a
Valentin Pozniak <waliker448@gmail.com>
2024-07-10 18:13:22 +0300

    add create button for tag_list.html

--- tag/urls.py
+++ tag/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from tag.views import TagListView
+from tag.views import TagListView, TagCreateView
 
 urlpatterns = [
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
 ]
 
 app_name = "tag"
--- tag/views.py
+++ tag/views.py
@@ -1,7 +1,15 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
+from tag.forms import TagForm
 from tag.models import Tag
 
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("tag:tag-list")
