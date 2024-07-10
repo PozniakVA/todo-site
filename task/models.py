a6cd56bc12a602008860be69d4be3a230541d849
Valentin Pozniak <waliker448@gmail.com>
2024-07-10 16:06:44 +0300

    create Task model

--- task/models.py
+++ task/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from tag.models import Tag
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="tasks",
+    )
+
+    class Meta:
+        ordering = ("-datetime",)
+
+    def __str__(self):
+        return self.content
+
